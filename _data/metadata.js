5099b290ea15914ecf2a0c1a332e7e563a98e710
Pavel Dolgov <gotterbild23@gmail.com>
2024-09-08 23:11:42 -0500

    First commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Паша долго пишет",
+	url: "https://pashapolgopishet.com/",
+	language: "ru",
+	description: "Личный блог. Пишу про то, что мне интересно: IT, путешествия иногда немного политоты. В данный момент живу в Перу.",
+	author: {
+		name: "Павел Д.",
+		email: "gtb-blog@pm.me",
+		url: "https://pashapolgopishet.com/about-me/"
+	}
+}
---
+++ content/about/index.md
@@ -0,0 +1,9 @@
+---
+layout: layouts/base.njk
+eleventyNavigation:
+  key: Обо мне
+  order: 3
+---
+# Обо мне
+
+Написать что-то
