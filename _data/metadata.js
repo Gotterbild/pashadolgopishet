074694ca1c434615567fd9801c07df5d0afe076a
Pavel Dolgov <gotterbild23@gmail.com>
2024-09-14 00:47:31 -0500

    Update metadata and fix links

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,10 @@
 module.exports = {
 	title: "Паша долго пишет",
-	url: "https://pashapolgopishet.com/",
+	url: "https://pashapolgopishet.blog/",
 	language: "ru",
 	description: "Личный блог. Пишу про то, что мне интересно: IT, путешествия иногда немного политоты. В данный момент живу в Перу.",
 	author: {
 		name: "Павел Д.",
 		email: "gtb-blog@pm.me",
-		url: "https://pashapolgopishet.com/about-me/"
 	}
 }
