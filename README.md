30da7802de6a421899fe684c3a54f52000efcbbf
Pavel Dolgov <gotterbild23@gmail.com>
2024-09-27 21:03:02 -0500

    Add redirects.js

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 - Добавить иконуц RSS
 - Добавить статьи о прибытии в Перу и из папки Лима
 - Пагинация???
+- добвить редиректы, хотя бы на статьи про ВНЖ
 - Поменять Plausible на
 	- Simple Analytics https://www.simpleanalytics.com/
 	- Insights https://getinsights.io/#pricing
---
+++ _data/redirects.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { 
+    from: '/blog/2024-06-09-prodlenie-vnzh-peru/', 
+    to:   '/blog/2024/2024-06-09-prodlenie-vnzh-peru/', 
+  },
+  { 
+    from: '/blog/2023-10-28-poluchenie-vnzh-peru/', 
+    to:   '/blog/2023/2023-10-28-poluchenie-vnzh-peru/', 
+  },
+]
